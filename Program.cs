8b7070e348da091e452e20188b9900eabf010c3a
IDGS-801-23000387 <86535@alumnos.utleon.edu.mx>
2025-11-06 14:20:53 -0600

    Correcion

--- Program.cs
+++ Program.cs
@@ -7,19 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
+if (string.IsNullOrEmpty(connectionString))
+{
+    Console.WriteLine(" No se encontró la cadena de conexión a la base de datos.");
+}
+else
+{
+    Console.WriteLine($" Conectando a la base de datos: {connectionString}");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
 
+ 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+ 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("netlify", policy =>
     {
         policy.WithOrigins(
-            "http://localhost:5173",
-            "https://storeonlines.netlify.app"
+            "http://localhost:5173",                  
+            "https://storeonlines.netlify.app"        
         )
         .AllowAnyHeader()
         .AllowAnyMethod();
@@ -27,20 +38,21 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
+
+ 
+if (app.Environment.IsDevelopment())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "StoreOnline API v1");
+        c.RoutePrefix = string.Empty;  
+    });
 }
 
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "StoreOnline API v1");
-    c.RoutePrefix = string.Empty;
-});
 app.UseHttpsRedirection();
 app.UseCors("netlify");
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
